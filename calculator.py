0c4a59d6c7bb0966d730a2fef1e1540e4c3fd9ef
Olga <hijazin.olga@gmail.com>
2020-02-05 12:54:20 -0800

    Do sum sub div and mul

--- calculator.py
+++ calculator.py
@@ -1,8 +1,28 @@
 
 def main():
 	#write your code here
+	first_number = input("Enter the first number: ")
+	second_number = input("Enter the second number: ")
+	operation = input("Choose the operation (+, -, /, *): ")
+
+	if  not first_number.isdigit() or not second_number.isdigit():
+		print("The numbers are not valid")
+	else:
+		if operation == '*':
+				print("The answer is ", int(first_number)*int(second_number))
+		elif operation == '+':
+				print("The answer is ", int(first_number)+int(second_number))
+		elif operation == '-':
+				print("The answer is ", int(first_number)-int(second_number))
+		elif operation =='/':
+				print("The answer is ", int(first_number)/int(second_number))
+		else:
+			print("The operation is not valid")
+
+
+
 	pass
-	
+
 
 
 
